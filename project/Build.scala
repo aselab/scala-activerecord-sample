1de26f55b73967b7706557a4bdfe61d20397a9f7
y-yoshinoya <y2.void+github@gmail.com>
2012-04-16 15:31:50 +0900

    forget add

--- project/Build.scala
+++ project/Build.scala
@@ -2,20 +2,5 @@ import sbt._
 import Keys._
 
 object ScalaActiveRecordSample extends Build {
-  lazy val simple = Project("simple", file("simple/"), 
-    settings = Project.defaultSettings ++ Seq(
-      name := "scala-activerecord-sample",
-      organization := "com.github.aselab",
-      version := "0.1",
-      resolvers ++= Seq(
-        "aselab repo" at "http://aselab.github.com/maven/",
-        "typesafe repo" at "http://repo.typesafe.com/typesafe/releases/"
-      ),
-      libraryDependencies ++= Seq(
-        "com.github.aselab" % "scala-activerecord" % "0.1-SNAPSHOT",
-        "com.h2database" % "h2" % "1.3.157",
-        "org.slf4j" % "slf4j-nop" % "1.6.4"
-      ),
-      scalacOptions ++= Seq("-deprecation", "-unchecked")
-    ))
+  lazy val simple = Project("simple", file("simple/"))
 }
