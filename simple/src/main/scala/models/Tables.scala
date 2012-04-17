586783d02c9ff0a19cfe59a67f245e99cad0527f
y-yoshinoya <y2.void+github@gmail.com>
2012-04-17 10:05:53 +0900

    change table name

--- simple/src/main/scala/models/Tables.scala
+++ simple/src/main/scala/models/Tables.scala
@@ -7,6 +7,5 @@ import com.github.aselab.activerecord.dsl._
  * Table definition.
  */
 object Tables extends ActiveRecordTables {
-  // convention: table name must be classNameTable
-  val userTable = table[User]
+  val users = table[User]
 }
