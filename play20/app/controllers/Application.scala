aa36defeeb22b0b063c8e187f28ed9c329e1f8c0
y-yoshinoya <y2.void+github@gmail.com>
2012-05-02 20:34:58 +0900

    tuning parts

--- play20/app/controllers/Application.scala
+++ play20/app/controllers/Application.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Hello, ScalaActiverecord"))
+    Ok(views.html.index("Hello, Scala ActiveRecord"))
   }
 
 }
