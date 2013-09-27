17eb3156b9ea8fe26ff285ae1e45711bb981f58c
y-yoshinoya <y2.void+github@gmail.com>
2013-09-28 02:37:39 +0900

    remove unused files

--- play2x/app/views/helpers/BootstrapHelper.scala
+++
@@ -1,8 +0,0 @@
-package views.helpers
-
-import views.html.helpers.bootstrapField
-import views.html.helper.FieldConstructor
-
-object BootstrapHelper {
-  implicit val bootstrapConstructor = FieldConstructor(bootstrapField.f)
-}
