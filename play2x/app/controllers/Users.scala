f055f9c326a9774f2b5aad9112898641d425eaab
Akihiro Ono <akihiro.ase@gmail.com>
2013-09-29 20:00:31 +0900

    Updated play2x sample

--- play2x/app/controllers/Users.scala
+++ play2x/app/controllers/Users.scala
@@ -29,7 +29,7 @@ object Users extends Controller {
     User.form.bindFromRequest.fold(
       errors => BadRequest(view.edit(errors, routes.Users.create, "Create", "User create")), {
       user =>
-        transaction { user.saveWithoutValidation }
+        transaction { user.save }
         Redirect(routes.Users.show(user.id))
     })
   }
@@ -47,7 +47,7 @@ object Users extends Controller {
         User.form(user).bindFromRequest.fold(
           errors => BadRequest(view.edit(errors, routes.Users.update(id), "Update", "User edit")), {
           user =>
-            transaction { user.saveWithoutValidation }
+            transaction { user.save }
             Redirect(routes.Users.index)
         })
       case _ => NotFound
